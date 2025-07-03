9146ffd626b3b3287c52a56603b1b19f770a242b
amitmishra-20 <amitmishra20900@gmail.com>
2025-07-04 03:50:40 +0530

    Add process.env.PORT support for deployment flexibility

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -160,5 +160,5 @@ wss.on('connection', (ws) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
